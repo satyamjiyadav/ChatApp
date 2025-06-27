7dd86f37e899e5d35d836a106070f3bec8812693
satyamjiyadav <satyamjiyadav12345@gmail.com>
2025-06-27 19:35:43 +0530

    fixed mismach

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io");
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.SKT_URL,
     methods: ["GET", "POST"],
   },
 });
